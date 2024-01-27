1a2f9df9383098ebab6e5227340ce51636c51ed0
Noah Fuery <nfuery@chapman.edu>
2024-01-26 23:35:58 -0800

    Finalizing

--- ExtraCredit/NFAtoDFA.py
+++ ExtraCredit/NFAtoDFA.py
@@ -1,13 +1,8 @@
 import argparse
-import re
 from itertools import combinations
 
-# Removed unused State class
-
+# Defining class that can be used for both nfas and dfas
 class Automaton:
-    """
-    A class to represent a generic automaton (NFA or DFA).
-    """
     def __init__(self, states, symbols, start_state, accept_states, transitions):
         self.states = states
         self.symbols = symbols
@@ -15,100 +10,145 @@ class Automaton:
         self.accept_states = accept_states
         self.transitions = transitions
 
+# Reading through the input file to get data about nfa
 def read_nfa(file_name):
-    """
-    Reads a NFA from a file.
-    """
-    with open(file_name, 'r') as file:
-        lines = file.readlines()
-        states = lines[0].strip().split('\t')
-        symbols = lines[1].strip().split('\t')
-        start_state = lines[2].strip()
-        accept_states = lines[3].strip().split('\t')
-        transitions = {line.strip().split(', ')[0]: line.strip().split(' = ')[1] for line in lines[5:-1]}
-        return Automaton(states, symbols, start_state, accept_states, transitions)
+    file = open(file_name, 'r')
+    lines = file.readlines()
 
+    states = []
+    for state in lines[0].strip().split('\t'):
+        states.append(state.strip('{}')) # Ignoring brackets to make storing easier
+
+    symbols = lines[1].strip().split('\t')
+
+    start_state = lines[2].split()[0][1]
+
+    accept_states = []
+    for state in lines[3].strip().split('\t'):
+        accept_states.append(state.strip('{}'))
+
+    # Where I made a mistake of using a dictionary to store transitions. I am not sure what structure I could have used instead of a dictionary
+    # but I thought it would work.
+    transitions = {}
+    for line in lines[5:-1]:
+        line_half1 = line.split(' = ')[0]
+        line_half2 = line.split(' = ')[1]
+        initial_state = line_half1.split(', ')[0].strip('{}')
+        symbol = line_half1.split(', ')[1]
+        next_state = line_half2.split()[0].strip('{}')
+        
+        transitions[initial_state + symbol] = next_state
+
+    return Automaton(states, symbols, start_state, accept_states, transitions)
+
+# Code to perform conversion
 def convert_nfa_to_dfa(nfa):
-    """
-    Converts a given NFA to a DFA.
-    """
-    # Create a list of all possible states (subsets of the NFA states)
+    # power set of all states
     dfa_states = []
     for i in range(len(nfa.states) + 1):
         for states in combinations(nfa.states, i):
-            dfa_states.append(set(states))
-    
-    # Initialize the DFA transitions as an empty dictionary
+            dfa_states.append(list(states))
+
+    # Epsilon transition on start state if applicable
+    dfa_start_state = []
+    dfa_start_state.append(nfa.start_state)
+    eps_transition = epsilon_transition(nfa, nfa.start_state)
+    for eps in eps_transition:
+        dfa_start_state.append(eps)
+
+    # Transitions of dfa
     dfa_transitions = {}
+    curr_state = nfa.start_state
+    temp_transitions = nfa.transitions.copy() # Wanted to have transitions removed after each use for each symbol iteration
+    for state in nfa.states: # Going through the possible states from nfa
+        temp_transitions = nfa.transitions.copy() # Resetting transition pool
+        for symbol in nfa.symbols: # Going through each symbol
+            new_dfa_state = [] # Initializing next state
+            for s in curr_state: # Going through the current states to check for each new possible transition
+                # Checking if the current state has an available transition and does not overlap with previously made transitions
+                if ((nfa.transitions.get(s + symbol) != None) and (nfa.transitions.get(s + symbol) not in new_dfa_state)): 
+                    new_dfa_state.append(nfa.transitions.get(s + symbol))
+                    temp_transitions.pop(s+symbol)
+                # Performing an epsilon transition if applicable
+                elif (epsilon_transition(nfa, s) != []):
+                    new_dfa_state.append(s)
+                    new_dfa_state.append(nfa.transitions.get(s + "EPS"))
+                    temp_transitions.pop(s+"EPS")
+                
+            new_dfa_state.sort()
+            # To make sure output file has accurate start state in transitions
+            if(curr_state == nfa.start_state):
+                dfa_transitions[(tuple(dfa_start_state), symbol)] = new_dfa_state
+            else:
+                dfa_transitions[(tuple(curr_state), symbol)] = new_dfa_state
+        curr_state = new_dfa_state # Updating current state with each pass, would also need to fix this
 
-    # For each DFA state (which is a subset of NFA states)
-    for dfa_state in dfa_states:
-        # For each symbol in the alphabet
-        for symbol in nfa.symbols:
-            # The new DFA state is the union of the NFA transitions for each state in the subset
-            new_dfa_state = set()
-            for state in dfa_state:
-                new_dfa_state = new_dfa_state.union(nfa.transitions.get((state, symbol), set()))
-            # Add the transition to the DFA transitions
-            dfa_transitions[(frozenset(dfa_state), symbol)] = new_dfa_state
-
-    # The start state of the DFA is the same as the NFA
-    dfa_start_state = nfa.start_state
-
-    # The accept states of the DFA are those that contain at least one NFA accept state
+    # Accept states are wrong but I would implement the transitions to get them if I had accurate transitions
     dfa_accept_states = []
-    for dfa_state in dfa_states:
-        if any(state in dfa_state for state in nfa.accept_states):
-            dfa_accept_states.append(dfa_state)
+    for state in dfa_states:
+        for s in nfa.accept_states:
+            if (s in state and s in dfa_start_state):
+                dfa_accept_states.append(tuple(state))
 
-    # Create the DFA
     dfa = Automaton(dfa_states, nfa.symbols, dfa_start_state, dfa_accept_states, dfa_transitions)
 
     return dfa
 
+# Method to see if there is a epsilon transition and if so perform it
+def epsilon_transition(nfa, state):
+    transition = [] # Stores all previous transitions
+    stack = [state] # Stack of the current state
+
+    while len(stack) > 0: # Looping until the stack is empty
+        current_state = stack.pop()
+        
+        epsilon_transitions = nfa.transitions.get((current_state + 'EPS'), []) # Gets the corresponding value with the state and EPS, i.e. '1EPS'
+        for next_state in epsilon_transitions: 
+            if next_state not in transition: # Continuing epsilon transition possibilities
+                transition.append(next_state)
+                stack.append(next_state)
+
+    return transition # Returning list of possible transitions
+
+# Writing to output file
 def write_dfa(automaton, file_name):
-    """
-    Writes a given DFA to a file.
-    """
-    with open(file_name, 'w') as file:
+    file = open(file_name, 'w')
+    for state in automaton.states:
+        # Formatting for {EM}
+        if len(state) == 0:
+            file.write('{EM}\t')
+        else:
+            file.write('{' + ', '.join(map(str, state)) + '}\t')
+    file.write('\n')
 
-        for state in automaton.states:
-            if(len(state) == 0):
-                file.write(', '.join(['{EM}']))
-                file.write('\t')
-            else:
-                # file.write(', '.join(map(str,state)))
-                print(', '.join(str(state[0])))
-                file.write('\t')
-        file.write('\n')
+    file.write('\t'.join(automaton.symbols) + '\n')
+    file.write('{' + ', '.join(automaton.start_state) + '}\n')
+    for acceptState in automaton.accept_states:
+        file.write('{' + (', '.join(map(str, acceptState))) + '}\t')
+    file.write('\n')
 
-        file.write('\t'.join(automaton.symbols) + '\n')
-        file.write(', '.join(automaton.start_state) + '\n')
-
-        for state in automaton.accept_states:
-            file.write(', '.join(state))
-        file.write('\n')
-
-        file.write('BEGIN\n')
-        for (state, symbol), transitions in automaton.transitions.items():
-            if (len(state) == 0 and len(transitions) == 0):
-                file.write(', '.join(['{EM}']) + ', ' + symbol + ' = ' + ', '.join(['{EM}']) + '\n') 
-            elif (len(state) == 0):
-                file.write(', '.join(['{EM}']) + ', ' + symbol + ' = ' + ', '.join(transitions) + '\n')
-            elif (len(transitions) == 0):
-                file.write(', '.join(state) + ', ' + symbol + ' = ' + ', '.join(['{EM}']) + '\n')
-            else:
-                file.write(', '.join(state) + ', ' + symbol + ' = ' + ', '.join(transitions) + '\n')
-        file.write('END\n')
+    file.write('BEGIN\n')
+    for (state, symbol), transitions in automaton.transitions.items():
+        # All if statements below to check if there is an empty array and inputting {EM} for correct formatting
+        if len(state) == 0 and len(transitions) == 0:
+            file.write('{EM}, ' + symbol + ' = ' + '{EM}\n')
+        elif len(state) == 0:
+            file.write('{EM}, ' + symbol + ' = ' + '{' + ', '.join(transitions) + '}\n')
+        elif len(transitions) == 0:
+            file.write('{' + ', '.join(map(str, state)) + '}, ' + symbol + ' = ' + '{EM}\n')
+        else:
+            file.write('{' + ', '.join(map(str, state)) + '}, ' + symbol + ' = ' + '{' + ', '.join(transitions) + '}\n')
+    file.write('END\n')
 
 def main():
+    # parser to allow for CLI
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_file', help='The input file containing the NFA specification')
-    args = parser.parse_args()
-
+    parser.add_argument('input_file') # Input file on line
+    args = parser.parse_args() # Args to read input file
     nfa = read_nfa(args.input_file)
     dfa = convert_nfa_to_dfa(nfa)
-    write_dfa(dfa, args.input_file.replace('.nfa', '.DFA'))
+    output_file = 'output.DFA' # Choosing output file name
+    write_dfa(dfa, output_file)
 
 if __name__ == '__main__':
     main()
--- ExtraCredit/README.md
+++ ExtraCredit/README.md
@@ -1,5 +1,13 @@
+Noah Fuery
+1/26/2024
+CPSC 406 - Algorithm Analysis
+Dr. Erik Linstead
 
+How to run program on command lin:
+python3 NFAtoDFA.py "Name of input file"
 
+Difficulties and Successes:
+I managed to get most of the logic complete for the transitions and reading and writing the files. I struggled a lot with the dfa transitions as I realized way too late that my nfa transitions were not all being stored as I was using a dictionary to store them, so each key was storing only one value. If I had more time I would fix this. I also had trouble finding the accept states due to the fact that they are dependent on the transitions being done. Another problem I spent a long time on was handling multiple transition options for each state, i.e. if state {2} could go to either itself with a or {3} with a. 
 
 Sources:
 https://www.geeksforgeeks.org/command-line-interface-programming-python/
